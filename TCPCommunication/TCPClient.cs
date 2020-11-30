f5919f4d550120b7c6333a8dcb5bba2697aad2ac
ssm8887 <ssm8887@naver.com>
2020-11-30 15:23:50 +0900

    Add project files.

---
+++ TCPCommunication/TCPClient.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPCommunication
+{
+    class TCPClient
+    {
+        private string ipAddress;
+        private int port;
+        private byte[] buff = new byte[1024];
+        private TcpClient client;
+        private NetworkStream networkStream;
+
+        private CancellationTokenSource tokenSourceExecute = null;
+        private CancellationTokenSource tokenSourceReceive = null;
+
+        public event Action<byte[]> receiveAction;
+
+        public TCPClient(string ipAddress, int port)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+
+        public void Connect()
+        {
+            if (client is null)
+            {
+                client = new TcpClient();
+            }
+
+            tokenSourceExecute = new CancellationTokenSource();
+
+            Task task = null;
+            IPAddress ip = null;
+
+            if (IPAddress.TryParse(ipAddress, out var ipAddr))
+            {
+                ip = ipAddr;
+            }
+            while (!tokenSourceExecute.Token.IsCancellationRequested)
+            {
+                try
+                {
+                    task = client.ConnectAsync(ip, port);
+                    task.Wait(5000);
+
+                    if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        task = null;
+
+                        if (networkStream != null)
+                        {
+                            DisConnect(ref client);
+                        }
+
+                        networkStream = client?.GetStream();
+                        Receive();
+                        break;
+                    }
+                    else
+                    {
+                        task = null;
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public async void Receive()
+        {
+            int rcvBytes = 0;
+            this.tokenSourceReceive = new CancellationTokenSource();
+
+            while (!this.tokenSourceReceive.Token.IsCancellationRequested)
+            {
+
+                try
+                {
+                    rcvBytes = await networkStream.ReadAsync(this.buff, 0, this.buff.Length).ConfigureAwait(false);
+
+                    if (rcvBytes == 0)
+                    {
+                        this.DisConnect(ref client);
+                        break;
+                    }
+                    else if (rcvBytes == 30)
+                    {
+                        this.receiveAction?.Invoke(buff);
+                        rcvBytes = 0;
+                    }
+                    else
+                    {
+                        rcvBytes = 0;
+                    }
+                }
+                catch
+                {
+                    this.DisConnect(ref client);
+                    break;
+                }
+
+            }
+        }
+
+        private void DisConnect(ref TcpClient tempclient)
+        {
+            if (tempclient != null)
+            {
+                tempclient.Close();
+                tempclient = null;
+            }
+
+            if (networkStream != null)
+            {
+                networkStream.Close();
+                networkStream = null;
+            }
+
+        }
+
+        public void RequestCancel()
+        {
+            this.tokenSourceExecute?.Cancel();
+            this.tokenSourceReceive?.Cancel();
+            this.DisConnect(ref this.client);
+
+            this.client = null;
+        }
+
+    }
+}
---
+++ TCPCommunication/TCPServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TCPCommunication
+{
+    public class TCPServer
+    {
+        private string ipAddress;
+        private int port;
+
+        byte[] buff = new byte[1024];
+
+        public event Action<byte[]> receiveAction;
+
+        public TCPServer(string ipAddress, int port)
+        {
+            this.ipAddress = ipAddress;
+            this.port = port;
+        }
+
+        public void Connect()
+        {
+            AsyncServer().Wait();
+        }
+
+        private async Task AsyncServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse(ipAddress), port);
+            listener.Start();
+
+            while (true)
+            {
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                await Task.Run(() => AsyncTCPProcess(tc));
+            }
+        }
+
+        private async void AsyncTCPProcess(Object o)
+        {
+            TcpClient tc = o as TcpClient;
+
+            var stream = tc.GetStream();
+
+            int recvBytes = 0;
+
+            while (true)
+            {
+                try
+                {
+                    recvBytes = await stream.ReadAsync(buff, 0, buff.Length);
+                }
+                catch
+                {
+                    Console.WriteLine("ReadAsync Error");
+                }
+
+                if (recvBytes == 0)
+                {
+                    stream.Close();
+                }
+                else
+                {
+                    receiveAction?.Invoke(buff);
+                }
+
+            }
+        }
+
+    }
+}
