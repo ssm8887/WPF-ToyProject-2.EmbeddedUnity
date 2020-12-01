a961f7ac4b81e3a79b288c654f7e38d4116dc924
ssm8887 <ssm8887@naver.com>
2020-12-01 16:59:39 +0900

    Unity 내장 WPF 추가

---
+++ UnityEmbeddedWPF/MainWindow.xaml.EmbeddedUnity.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Threading;
+
+namespace UnityEmbeddedWPF
+{
+    public partial class MainWindow
+    {
+        [DllImport("User32.dll")]
+        static extern bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+
+        internal delegate int WindowEnumProc(IntPtr hwnd, IntPtr lparam);
+        [DllImport("user32.dll")]
+        internal static extern bool EnumChildWindows(IntPtr hwnd, WindowEnumProc func, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        private Process process;
+        private IntPtr unityHWND = IntPtr.Zero;
+
+        private const int WM_ACTIVATE = 0x0006;
+        private readonly IntPtr WA_ACTIVE = new IntPtr(1);
+        private readonly IntPtr WA_INACTIVE = new IntPtr(0);
+
+        private void UnityControl_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                process = new Process();
+                process.StartInfo.FileName = "ToyProject201127.exe";
+                process.StartInfo.Arguments = "-parentHWND " + this.UnityPanel.Handle.ToInt32() + " " + Environment.CommandLine;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+
+                process.WaitForInputIdle();
+
+                EnumChildWindows(this.UnityPanel.Handle, WindowEnum, IntPtr.Zero);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message + ".\nCheck if Container.exe is placed next to Child.exe.");
+
+            }
+        }
+
+        private void ActivateUnityWindow()
+        {
+            SendMessage(unityHWND, WM_ACTIVATE, WA_ACTIVE, IntPtr.Zero);
+        }
+
+        private void DeactivateUnityWindow()
+        {
+            SendMessage(unityHWND, WM_ACTIVATE, WA_INACTIVE, IntPtr.Zero);
+        }
+
+        private int WindowEnum(IntPtr hwnd, IntPtr lparam)
+        {
+            unityHWND = hwnd;
+            ActivateUnityWindow();
+            return 0;
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                process.CloseMainWindow();
+
+                Thread.Sleep(1000);
+                while (process.HasExited == false)
+                    process.Kill();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            ActivateUnityWindow();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            DeactivateUnityWindow();
+        }
+    }
+}
--- UnityEmbeddedWPF/MainWindow.xaml.cs
+++ UnityEmbeddedWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +10,14 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using System.Threading;
 
 namespace UnityEmbeddedWPF
 {
@@ -23,6 +29,30 @@ namespace UnityEmbeddedWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += UnityControl_Load;
+            Activated += Form1_Activated;
+            Deactivated += Form1_Deactivate;
+            Closed += Form1_Closed;
+
+            Command_01.Click += Command_01_Click;
+            Command_02.Click += Command_02_Click;
+            Command_03.Click += Command_03_Click;
+        }
+
+        private void Command_01_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Command_02_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Command_03_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
